25207ea7262b05e73f43ca3c6600c82d244df71d
Nisar Ahmed <nisarahmed.qae@gmail.com>
2026-08-21 21:41:10 +0530

    position updated

--- JavaLogics/src/main/java/position/ThirdLargestUniqueNumber.java
+++ JavaLogics/src/main/java/position/ThirdLargestUniqueNumber.java
@@ -1,32 +1,30 @@
 package position;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
+import java.util.*;
 
 public class ThirdLargestUniqueNumber {
 
-	public static void main(String[] args) {
-		int[] arr = { 4, 3, 2, 7, 3, 4, 8 };
-		HashMap<Integer, Integer> hashmap = new HashMap<>();
-		ArrayList<Integer> list = new ArrayList<>();
+    public static void main(String[] args) {
+        int[] arr = {4, 3, 2, 7, 3, 4, 8};
+        Map<Integer, Integer> map = new HashMap<>();
+        List<Integer> list = new ArrayList<>();
 
-		for (int ele : arr) {
-			// Increment count if the element exists, else initialize with 1
-			hashmap.put(ele, hashmap.getOrDefault(ele, 0) + 1);
-		}
-		System.out.println(hashmap);
+        for (int ele : arr) {
+            // Increment count if the element exists, else initialize with 1
+            map.merge(ele, 1, Integer::sum);
+        }
+        System.out.println(map); // {2=1, 3=2, 4=2, 7=1, 8=1}
 
-		for (int ele : hashmap.keySet()) {
-			if (hashmap.get(ele) == 1) {
-				list.add(ele);
-			}
-		}
+        for (int ele : map.keySet()) {
+            if (map.get(ele) == 1) {
+                list.add(ele);
+            }
+        }
 
-		System.out.println(list);
-		Collections.sort(list, Collections.reverseOrder());
-		System.out.println(list);
-		System.out.println(list.get(2)); // Third Highest Number according to Index
-	}
+        System.out.println(list); // [2, 7, 8]
+        Collections.sort(list, Collections.reverseOrder());
+        System.out.println(list); // [8, 7, 2]
+        System.out.println(list.get(2)); // Third Highest Number(2) according to Index
+    }
 
 }
